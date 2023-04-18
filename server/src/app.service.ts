95ff1422bd7fa7830bd8c143add19adf55486167
Yegor Arndt <devegorarndt@gmail.com>
2023-04-18 16:21:44 +0400

    Testing env, aws db

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Connection } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(private connection: Connection) {}
+
+  async onModuleInit() {
+    const isConnected = this.connection.isConnected;
+    if (isConnected) {
+      console.log('Database connection established');
+    } else {
+      console.log('Database connection failed');
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
